a6800852da67609da44e5aa6348c47c1ecc950ba
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-05 12:36:55 +0200

    almost(the desk is working)

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-var text = document.createElement('div');
-text.className = "text rex";
-text.innerHTML = "Here you can see a chess desk";
-document.body.appendChild(text);
+var desk = document.createElement('div');
+desk.className = 'desk';
+document.body.appendChild(desk);
 
-var numblist = document.createElement('div');
-numblist.className = 'numbliste';
-numblist.innerHTML = ''+ 1;
-document.body.appendChild(numblist);
-
-for(var i = 2; i <= 8;i++){
-  var numblist = document.createElement('div');
-  numblist.className = 'numblist';
-  numblist.innerHTML = ''+ i;
-  document.body.appendChild(numblist);
+for (var i = 72; i >= 65;i--){
+var tabup = document.createElement('div');
+tabup.className = 'tabup';
+tabup.innerHTML = String.fromCharCode(i);
+desk.appendChild(tabup);
 }
-
-for(var i = 0 ;i < 8;i++){
-  if(i%2){
-  var black = document.createElement('div');
-  black.className = 'black';
-  document.body.appendChild(black);
-  }
-  else{
-   var white = document.createElement('div');
-  white.className = 'white';
-  document.body.appendChild(white); 
+for(var i=0;i<8;i++){
+  for(var j = 0;j<8;j++){
+    if((i+j)%2){
+      var black = document.createElement('div');
+      black.className = 'black';
+      desk.appendChild(black);
+    }
+    else{
+      var white = document.createElement('div');
+      white.className = 'white';
+      desk.appendChild(white);
+    }
   }
 }
 
-
-/*for (var i = 65; i <= 72;i++){
-  var abclist = document.createElement('div');
-  abclist.className = 'abclist';
-  abclist.innerHTML = String.fromCharCode(i);
-  document.body.appendChild(abclist);
-}*/
+for (var i = 65; i <= 72;i++){
+var tabdown = document.createElement('div');
+tabdown.className = 'tabdown';
+tabdown.innerHTML = String.fromCharCode(i);
+desk.appendChild(tabdown);
+}
